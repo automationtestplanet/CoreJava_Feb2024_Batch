5d01c96765e0d87c67e12ded1ab026a44f32627a
manojreddy <raju123@gmai.com>
2024-02-14 22:23:24 +0530

    Commited by ManojReddy Assignment2

--- src/assignments/manoj/Assignment2.java
+++ src/assignments/manoj/Assignment2.java
@@ -4,11 +4,32 @@ public class Assignment2 {
 	// Write a reusable program to check the number prime or not
 
 	public boolean checkPrime(int num) {
-		return false;
+		int n = num;
+		boolean isPrime = true;
+		if (n <= 1) {
+			isPrime = false;
+			return isPrime;
+		} else {
+			for (int i = 2; i < n; i++) {
+				if (n % i == 0) {
+					isPrime = false;
+					break;
+				}
+			}
+		}
+
+		return isPrime;
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int num = 101;
+		Assignment2 a2 = new Assignment2();
+		boolean isPrime = a2.checkPrime(num);
+		if (isPrime) {
+			System.out.println("Given number :" + num + " - is a Prime number.");
+		} else {
+			System.out.println("Given number :" + num + " - is not a Prime number.");
+		}
 
 	}
 }
