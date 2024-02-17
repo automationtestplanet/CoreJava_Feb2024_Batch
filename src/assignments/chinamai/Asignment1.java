48882d9a41a8fd765a36f1569234bbc8e7fc25c0
chinmai <chinmaiganta1997@gmail.com>
2024-02-17 09:33:14 +0530

    new assignment

--- src/assignments/chinamai/Asignment1.java
+++ src/assignments/chinamai/Asignment1.java
@@ -4,13 +4,27 @@ public class Asignment1 {
 
 	// Write a reusable program to check the number prime or not
 
-	public boolean checkPrime(int num) {
-		return false;
-	}
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int num =7;//example number to check for primality
+		if (checkPrime(num)) {
+			System.out.println(num+"is a prime number.");
+		}
+		else {
+			System.out.println(num+"is not prime number.");
+		}
 
+	}
+	public static boolean checkPrime(int num) {
+		if(num<=1) {
+			return false;
+		}
+		for (int i=2; i<=Math.sqrt(num);i++) {
+			if(num % i==0) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
