c3c1cdddd88c1509abced0034c034401261291eb
chinmai <chinmaiganta1997@gmail.com>
2024-02-21 17:24:18 +0530

    new number

--- src/assignments/chinamai/Asignment1.java
+++ src/assignments/chinamai/Asignment1.java
@@ -6,7 +6,7 @@ public class Asignment1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int num =7;//example number to check for primality
+		int num =15;//example number to check for primality
 		if (checkPrime(num)) {
 			System.out.println(num+"is a prime number.");
 		}
