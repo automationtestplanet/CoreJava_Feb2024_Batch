e665dec8e8da17effc4d57208f4cf275c12756e6
pavan <k.pavansubbareddy@gmail.com>
2024-02-24 17:19:17 +0530

    changes

--- src/assignments/nagapavan/Assignment2.java
+++ src/assignments/nagapavan/Assignment2.java
@@ -6,28 +6,28 @@ public class Assignment2 {
 	public boolean checkPrime(int num) {
 
 		int n = num;
-		boolean prime = true;
+		boolean primeNo = true;
 		if (n <= 1) {
-			prime = false;
-			return prime;
+			primeNo = false;
+			return primeNo;
 		} else {
 			for (int i = 2; i < n; i++) {
 				if (n % i == 0) {
-					prime = false;
+					primeNo = false;
 					break;
 				}
 			}
 		}
-		return prime;
+		return primeNo;
 	}
 
 	public static void main(String[] args) {
 		
 		int num=28;
 		Assignment2 ag=new Assignment2();
-		boolean prime =ag.checkPrime(num);
+		boolean primeNo =ag.checkPrime(num);
 		
-		if(prime) {
+		if(primeNo) {
 			System.out.println("prime number: "+num);
 		}else {
 			System.out.println("Not prime number: "+num);
