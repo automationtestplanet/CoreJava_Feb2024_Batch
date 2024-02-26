5171b976230acbbc8d56382ef9dafb63678fdf2b
chinmai <chinmaiganta1997@gmail.com>
2024-02-26 16:07:45 +0530

    new changes

--- src/assignments/chinamai/Assignment2.java
+++ src/assignments/chinamai/Assignment2.java
@@ -2,8 +2,6 @@ package assignments.chinamai;
 
 public class Assignment2 {
 	
-	//Write a program to to print Fibonacci Series
-		//Ex: 0,1,1,2,3,5,8,13...
 
 	public static void main(String[] args) {
 		int n = 10;//number of fibonacci numbers to generate
