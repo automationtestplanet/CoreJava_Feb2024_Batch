e7ff5a1bee027c57185273e22f382984f822b9f1
chinmai <chinmaiganta1997@gmail.com>
2024-02-26 15:57:03 +0530

    aasignment2 changes

--- src/assignments/chinamai/Assignment2.java
+++ src/assignments/chinamai/Assignment2.java
@@ -6,6 +6,20 @@ public class Assignment2 {
 		//Ex: 0,1,1,2,3,5,8,13...
 
 	public static void main(String[] args) {
+		int n = 10;//number of fibonacci numbers to generate
+		int firstNum = 0;
+		int secondNum = 1;
+		System.out.println("Fibonacci Series:");
+		System.out.print(firstNum+" "+secondNum);
+		
+		for(int i = 2;i<n;i++) {
+			int nextNum = firstNum+secondNum;
+			System.out.print(","+nextNum);
+			firstNum = secondNum;
+			secondNum = nextNum;
+		}
+		
+		
 		
 
 	}
