8251ff4db0ead23abdfba003f137460cb0db00a8
pavan <k.pavansubbareddy@gmail.com>
2024-02-26 12:55:18 +0530

    Assignment3

--- src/assignments/nagapavan/Assignment3.java
+++ src/assignments/nagapavan/Assignment3.java
@@ -1,12 +1,28 @@
 package assignments.nagapavan;
 
 public class Assignment3 {
-	
-	//Write a program to to print Fibonacci Series
-		//Ex: 0,1,1,2,3,5,8,13...
+
+	// Write a program to to print Fibonacci Series
+	// Ex: 0,1,1,2,3,5,8,13...
+
+	public void printFibonacci(int count) {
+
+		int n1 = 0, n2 = 1, n3;
+		if (count <= 2) {
+			System.out.println(n1 + " " + n2);
+		} else {
+			for (int i = 0; i < count; i++) {
+				n3 = n1 + n2;
+				System.out.println(" " + n3);
+				n1 = n2;
+				n2 = n3;
+			}
+		}
+	}
 
 	public static void main(String[] args) {
-		
+		Assignment3 ag = new Assignment3();
+		ag.printFibonacci(20);
 
 	}
 
