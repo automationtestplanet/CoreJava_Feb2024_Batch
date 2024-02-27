e32c782da20fd060e48b35ca8c20d52098b41042
ManojReddy <raju123@gmai.com>
2024-02-27 15:45:05 +0530

    manoj_assignment3 code changed

--- src/assignments/manoj/Assignment3.java
+++ src/assignments/manoj/Assignment3.java
@@ -1,30 +1,28 @@
 package assignments.manoj;
 
 public class Assignment3 {
-	
-	//Write a program to to print Fibonacci Series
-		//Ex: 0,1,1,2,3,5,8,13...
 
-public void  printFibonacci(int count) {
-		
-		int n1=0,n2=1,n3;
-		
-		if(count <=2) {
-			System.out.print(n1+" "+n2);
-		}else {
-			for(int i=0;i<count;i++) {
-				n3=n1+n2;
-				System.out.print(" "+n3);
-				n1=n2;
-				n2=n3;
-			}
+	// Write a program to to print Fibonacci Series
+	// Ex: 0,1,1,2,3,5,8,13...
+
+	public void printFibonacci(int count) {
+
+		int n1 = 0, n2 = 1, n3;
+
+		System.out.print(n1 + " " + n2);
+
+		for (int i = 0; i < count; i++) {
+			n3 = n1 + n2;
+			System.out.print(" " + n3);
+			n1 = n2;
+			n2 = n3;
 		}
-		
 	}
 
 	public static void main(String[] args) {
-		Assignment3 fn =new Assignment3();
-		fn.printFibonacci(20);
+		int count = 0;
+		Assignment3 fn = new Assignment3();
+		fn.printFibonacci(count-2);
 
 	}
 
