3e483c669dbc7fb7959dd77bd7fd0fa0b3d280ff
RAJU CHELLE <rajuchelle@gmail.com>
2024-03-02 17:43:01 +0530

    class 31 code

---
+++ src/other/InterviewQuesions.java
@@ -0,0 +1,18 @@
+package other;
+
+public class InterviewQuesions {
+
+	public static int getSumOfNumFromStr(String str) {
+		int sum = 0;
+		for (char eachChar : str.replaceAll("[^0-9]", "").toCharArray()) {
+			sum += ((int) eachChar) - '0';
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		String str1 = "page 44 of 99";
+		System.out.println(getSumOfNumFromStr(str1));
+	}
+
+}
